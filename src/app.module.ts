056d8a2ce274d66d676cc109b1fed47722565a18
贝贝 <3188517516@qq.com>
2023-06-17 00:04:40 +0800

    feat: 增加日志中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { LoggerMiddleWare } from './common/middleware/logger.middleware';
 import { HelloModule } from './modules/hello/hello.module';
 
 @Module({
   imports: [HelloModule],
 })
-export class AppModule {}
+export class AppModule {
+  //为hello添加日志中间件(排除post的方法)
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleWare)
+      .exclude({ path: 'hello', method: RequestMethod.POST })
+      .forRoutes('hello');
+  }
+}
---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,12 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+//注入
+@Injectable()
+export class LoggerMiddleWare implements NestMiddleware {
+  use(req: Request, res: Response, next: () => void) {
+    const { method, path } = req;
+    console.log(`${method}-${path}`);
+    next();
+  }
+}
